b6c8f2024e6ab5a61eb4fa4703e29260f66ef64c
ikmalfalahi <ikmalfalahi99@gmail.com>
2025-10-25 09:18:11 +0700

    Update produk.js

--- js/produk.js
+++ js/produk.js
@@ -1,6 +1,6 @@
 // === Supabase init ===
-const supabaseUrl = "https://nnohtnywmhuzueamsats.supabase.co";
-const supabaseKey = "YOUR_SUPABASE_ANON_KEY";
+const supabaseUrl = "https://ucizdtqovtajqjkgoyef.supabase.co";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVjaXpkdHFvdnRhanFqa2dveWVmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA4MTU5NjAsImV4cCI6MjA3NjM5MTk2MH0.c4CfIOGV6HqSTT_GkCZTSxjYfv5YmCHOMuMpXreRX8I";
 const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
 
 const productList = document.getElementById("product-list");
